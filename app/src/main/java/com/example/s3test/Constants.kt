06c2b1506669ca23ac6b5d73d861eedb83a8add4
Ji Seungmin <cswcsm02@gmail.com>
2022-12-22 16:45:33 +0900

    Apply .gitignore

--- app/src/main/java/com/example/s3test/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.s3test
-
-object Constants {
-
-    const val ACCESS_KEY = "AKIAQMPQF6QKIS7QWH5D"
-    const val ACCESS_SECRET_KEY = "GXpVCO8TyPi014jlHsQLmeZPZuAGu3KAXBkTv5l6"
-}
