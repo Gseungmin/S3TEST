ec9fae197fce08332f851e48da3d1df41e6f1b58
Ji Seungmin <cswcsm02@gmail.com>
2022-12-22 12:13:02 +0900

    갤러리 접근 및 사진 첨부 기능 완료

--- app/src/main/java/com/example/s3test/UploadActivity.kt
+++ app/src/main/java/com/example/s3test/UploadActivity.kt
@@ -1,10 +1,15 @@
 package com.example.s3test
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.s3test.databinding.ActivityUploadBinding
@@ -19,6 +24,7 @@ class UploadActivity : AppCompatActivity() {
     private val PICK_IMAGE_FROM_GALLERY = 1000
     private val PICK_IMAGE_FROM_GALLERY_PERMISSION = 1010
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
@@ -32,6 +38,24 @@ class UploadActivity : AppCompatActivity() {
             binding.recyclerView.adapter = imageRVAdapter
             binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         }
+
+        // 사진첨부 버튼 클릭 이벤트 구현
+        binding.btnShowGallery.setOnClickListener {
+            when {
+                // 갤러리 접근 권한이 있는 경우
+                ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> showGallery(this@UploadActivity)
+
+                // 갤러리 접근 권한이 없는 경우 && 교육용 팝업을 보여줘야 하는 경우
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> showPermissionContextPopup()
+
+                // 권한 요청 하기
+                else -> requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PICK_IMAGE_FROM_GALLERY_PERMISSION)
+            }
+        }
     }
 
     /**
@@ -41,5 +65,22 @@ class UploadActivity : AppCompatActivity() {
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         activity.startActivityForResult(intent, PICK_IMAGE_FROM_GALLERY)
+    }
+
+    /**
+     * 권한이 없을때 권한 등록 팝업 함수
+     * */
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PICK_IMAGE_FROM_GALLERY_PERMISSION)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
     }
 }
