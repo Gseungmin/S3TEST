68c7b0f47b5d26e7b7838897fce5fa0213d9ef8d
Ji Seungmin <cswcsm02@gmail.com>
2022-12-22 13:11:30 +0900

    S3 UTIL 클래스

--- app/src/main/java/com/example/s3test/S3Util.kt
+++ app/src/main/java/com/example/s3test/S3Util.kt
@@ -1,50 +1,150 @@
 package com.example.s3test
 
 import android.content.Context
-import android.graphics.Region
-import android.icu.util.TimeZone.getRegion
 import android.text.TextUtils
 import com.amazonaws.auth.AWSCredentials
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.metrics.AwsSdkMetrics.getRegion
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
-import com.amazonaws.regions.Region.getRegion
-import com.amazonaws.regions.RegionUtils.getRegion
+import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
-import org.jetbrains.annotations.Nullable
 import java.io.File
 
 
 class S3Util {
 
-    AWSCredentials awsCredentials = new BasicAWSCredentials("AccessKey", "Secret Key");	// IAM 생성하며 받은 것 입력
-    AmazonS3Client s3Client = new AmazonS3Client(awsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2));
 
-    TransferUtility transferUtility = TransferUtility.builder().s3Client(s3Client).context(getActivity().getApplicationContext()).build();
-    TransferNetworkLossHandler.getInstance(getActivity().getApplicationContext());
+    private var accessKey = "" // IAM AccessKey
 
-    TransferObserver uploadObserver = transferUtility.upload("talent-house-app/photo", fileName, file);	// (bucket api, file이름, file객체)
+    private var secretKey = "" // IAM SecretKey
 
-    uploadObserver.setTransferListener(new TransferListener() {
-        @Override
-        public void onStateChanged(int id, TransferState state) {
-            if (state == TransferState.COMPLETED) {
-                // Handle a completed upload
-            }
-        }
-        @Override
-        public void onProgressChanged(int id, long current, long total) {
-            int done = (int) (((double) current / total) * 100.0);
-            Log.d("MYTAG", "UPLOAD - - ID: $id, percent done = $done");
-        }
-        @Override
-        public void onError(int id, Exception ex) {
-            Log.d("MYTAG", "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
-        }
+    private var region // S3 Region
+            : Region? = null
+
+    /**
+     * 생성자 생성 시 초기 Region 설정 : AP_NORTHEAST_2
+     */
+    fun S3Util() {
+        region = Region.getRegion(Regions.AP_NORTHEAST_2)
     }
 
+    /**
+     * Overloading
+     */
+    fun uploadWithTransferUtility(
+        context: Context?,
+        bucketName: String, file: File,
+        listener: TransferListener?,
+    ) {
+        this.uploadWithTransferUtility(
+            context,
+            bucketName, null, file, null,
+            listener
+        )
+    }
+
+    /**
+     * Overloading
+     */
+    fun uploadWithTransferUtility(
+        context: Context?,
+        bucketName: String, folder: String?, file: File,
+        listener: TransferListener?,
+    ) {
+        this.uploadWithTransferUtility(
+            context,
+            bucketName, folder, file, null,
+            listener
+        )
+    }
+
+    /**
+     * S3 파일 업로드
+     *
+     * @param context    Context
+     * @param bucketName S3 버킷 이름(/(슬래쉬) 없이)
+     * @param folder     버킷 내 폴더 경로(/(슬래쉬) 맨 앞, 맨 뒤 없이)
+     * @param fileName   파일 이름
+     * @param file       Local 파일 경로
+     * @param listener   AWS S3 TransferListener
+     */
+    fun uploadWithTransferUtility(
+        context: Context?,
+        bucketName: String, folder: String?,
+        file: File, fileName: String?,
+        listener: TransferListener?,
+    ) {
+        require(!(TextUtils.isEmpty(accessKey) || TextUtils.isEmpty(secretKey))) { "AccessKey & SecretKey must be not null" }
+        val awsCredentials: AWSCredentials = BasicAWSCredentials(
+            accessKey, secretKey
+        )
+        val s3Client = AmazonS3Client(
+            awsCredentials, region
+        )
+        val transferUtility = TransferUtility.builder()
+            .s3Client(s3Client)
+            .context(context)
+            .build()
+        TransferNetworkLossHandler.getInstance(context)
+        val uploadObserver = transferUtility.upload(
+            if (TextUtils.isEmpty(folder)) bucketName else "$bucketName/$folder",
+            if (TextUtils.isEmpty(fileName)) file.name else fileName,
+            file
+        )
+        uploadObserver.setTransferListener(listener)
+    }
+
+    /**
+     * Access, Secret Key 설정
+     */
+    fun setKeys(accessKey: String, secretKey: String): S3Util? {
+        this.accessKey = accessKey
+        this.secretKey = secretKey
+        return this
+    }
+
+    /**
+     * Access Key 설정
+     */
+    fun setAccessKey(accessKey: String): S3Util? {
+        this.accessKey = accessKey
+        return this
+    }
+
+    /**
+     * Secret Key 설정
+     */
+    fun setSecretKey(secretKey: String): S3Util? {
+        this.secretKey = secretKey
+        return this
+    }
+
+    /**
+     * Region Enum 으로 Region 설정
+     */
+    fun setRegion(regionName: Regions?): S3Util? {
+        region = Region.getRegion(regionName)
+        return this
+    }
+
+    /**
+     * Region Class 로 Region 설정
+     */
+    fun setRegion(region: Region?): S3Util? {
+        this.region = region
+        return this
+    }
+
+    /**
+     * Singleton Pattern
+     */
+    fun getInstance(): S3Util? {
+        return LHolder.instance
+    }
+
+    private object LHolder {
+        val instance = S3Util()
+    }
 }
