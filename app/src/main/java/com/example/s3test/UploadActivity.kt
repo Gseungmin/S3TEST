b921d57140dbea4d512fedf1e9f8de0f4a9e2131
Ji Seungmin <cswcsm02@gmail.com>
2022-12-22 12:07:08 +0900

    갤러리로 이동하는 함수 작성

--- app/src/main/java/com/example/s3test/UploadActivity.kt
+++ app/src/main/java/com/example/s3test/UploadActivity.kt
@@ -1,7 +1,10 @@
 package com.example.s3test
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.s3test.databinding.ActivityUploadBinding
@@ -12,6 +15,9 @@ class UploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadBinding
     private lateinit var imageRVAdapter : ImageUploadAdapter
     lateinit var viewModel: ImageViewModel
+
+    private val PICK_IMAGE_FROM_GALLERY = 1000
+    private val PICK_IMAGE_FROM_GALLERY_PERMISSION = 1010
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +32,14 @@ class UploadActivity : AppCompatActivity() {
             binding.recyclerView.adapter = imageRVAdapter
             binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         }
+    }
+
+    /**
+     * 갤러리 화면으로 이동
+     * */
+    private fun showGallery(activity: Activity) {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        activity.startActivityForResult(intent, PICK_IMAGE_FROM_GALLERY)
     }
 }
